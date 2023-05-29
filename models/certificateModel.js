efe98afe57fb7c18b81ce82a0db3882d503cd6d5
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-05-29 16:13:24 +0700

    Modified certificate model

--- models/certificateModel.js
+++ models/certificateModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const { generateNameSlug } = require('../utils/slugify');
+
 // function
 function generateCertificateID() {
   const characters =
@@ -15,15 +17,6 @@ function generateCertificateID() {
   return certificateId;
 }
 
-// adding certificate category
-const certCategorySchema = new mongoose.Schema({
-  category: {
-    type: String,
-    required: [true, 'Mohon isi kategori untuk sertifikat'],
-    unique: true,
-  },
-});
-
 const certificateSchema = new mongoose.Schema(
   {
     certificateId: { type: String, unique: true },
@@ -33,8 +26,18 @@ const certificateSchema = new mongoose.Schema(
       required: [true, 'Mohon isi nama file untuk sertifikat'],
     },
     category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'CertCategory',
+      required: [true, 'Kategori harus ada di sertifikat'],
+    },
+    categoryName: {
       type: mongoose.Schema.Types.String,
       ref: 'CertCategory',
+      required: [true, 'Nama kategori harus ada di sertifikat'],
+    },
+    cerCategorySlug: {
+      type: String,
+      index: true,
     },
     recepient: {
       type: mongoose.Schema.Types.ObjectId,
@@ -66,16 +69,30 @@ certificateSchema.pre('save', function (next) {
   next();
 });
 
+certificateSchema.pre('save', function (next) {
+  if (this.isNew || this.isModified('categoryName')) {
+    this.cerCategorySlug = generateNameSlug(this.categoryName);
+  }
+  next();
+});
+
 certificateSchema.pre(/^find/, function (next) {
   this.populate('recepient').populate({
     path: 'recepient',
     select: 'firstName lastName emailAddress profileImage',
   });
 
+  this.populate('category').populate({
+    path: 'category',
+    select: 'categoryName cerCategorySlug',
+  });
+
   next();
 });
 
-const CertCategory = mongoose.model('CertCategory', certCategorySchema);
+// indexing
+certificateSchema.index({ cerCategorySlug: 1 });
+
 const Certificate = mongoose.model('Certificate', certificateSchema);
 
-module.exports = { Certificate, CertCategory };
+module.exports = Certificate;
