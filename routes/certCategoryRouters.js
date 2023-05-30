c822bfd45ff252d44405e465e50a693202ca503f
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-05-30 09:35:21 +0700

    Modified certificate category router

---
+++ routes/certCategoryRouters.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// controllers
+const authController = require('../controllers/authController');
+const certCategoryController = require('../controllers/certCategoryController');
+
+const router = express.Router();
+
+router.use(authController.protect);
+
+// certificate routers
+
+router.use(authController.restrictTo('Publisher'));
+
+// certificate category
+router
+  .route('/')
+  .get(certCategoryController.getAllCertCategories)
+  .post(certCategoryController.addCertCategory);
+
+module.exports = router;
