5222ce4e167571d0d02d2876f75aab33c2cf0f74
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-28 10:31:32 +0700

    Updated certificate category controller

--- controllers/certCategoryController.js
+++ controllers/certCategoryController.js
@@ -14,3 +14,15 @@ exports.addCertCategory = factory.createOne(
   CertCategory,
   'Added category successful'
 );
+
+// update certificate category
+exports.editCertCategory = factory.updateOne(
+  CertCategory,
+  'Updated category successful'
+);
+
+// delete category
+exports.deleteCertCategory = factory.deleteOne(
+  CertCategory,
+  'Delete category successful'
+);
